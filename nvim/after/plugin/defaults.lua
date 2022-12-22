e2f621e72d5054ff0db6321522f429a2c838b7f1
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2022-12-22 12:54:19 +0100

    More options for neovim

--- nvim/after/plugin/defaults.lua
+++ nvim/after/plugin/defaults.lua
@@ -6,6 +6,24 @@ require('packer').startup(function(use)
 		as = 'rose-pine',
 		config = function()
 		vim.cmd('colorscheme rose-pine')
-		end
-	})
+	end
+})
 end)
+
+-- Cursor stays in middle when moving up and down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Let visual block move up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Leave lines and start scrolling earlier
+vim.opt.scrolloff = 8
+
+-- Indentation
+vim.opt.tabstop=2
+vim.opt.softtabstop=2
+vim.opt.expandtab=true
+vim.opt.smartindent=true
+
