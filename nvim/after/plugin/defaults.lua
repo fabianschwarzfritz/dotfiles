d36040bcedd13c8cedfbfb9f8b95a0b9fb314850
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2023-01-19 18:19:40 +0100

    Telescope: ignore node_modules

--- nvim/after/plugin/defaults.lua
+++ nvim/after/plugin/defaults.lua
@@ -25,5 +25,5 @@ vim.opt.scrolloff = 8
 vim.opt.tabstop=2
 vim.opt.softtabstop=2
 vim.opt.expandtab=true
-vim.opt.smartindent=true
+-- vim.opt.smartindent=true
 
--- nvim/init.lua
+++ nvim/init.lua
@@ -194,6 +194,7 @@ require('telescope').setup {
         ['<C-d>'] = false,
       },
     },
+    file_ignore_patterns = { "node_modules" }
   },
 }
 
